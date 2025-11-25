44f0173cc8976dc73e13f07bd3b596570e2307ce
Niragi Aushadhini <niragiaushadhini@gmail.com>
2025-11-25 16:29:34 +0530

    humidity esp code added

---
+++ components testing code/humidity_esp/humidity_esp.ino
@@ -0,0 +1,28 @@
+#include <DHT.h>
+
+#define DHTPIN D4
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("DHT ERROR");
+    return;
+  }
+
+  Serial.print("Temp: ");
+  Serial.print(t);
+  Serial.print("C | Humidity: ");
+  Serial.print(h);
+  Serial.println("%");
+
+  delay(2000);
+}
