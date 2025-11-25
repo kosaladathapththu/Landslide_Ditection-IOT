a1e353571b7d9f0b47abd0b44eabb778c73179ce
Niragi Aushadhini <niragiaushadhini@gmail.com>
2025-11-25 16:32:41 +0530

    soil arduino code added

---
+++ components testing code/Soil_Moisture_Arduino/Soil_Moisture_Arduino.ino
@@ -0,0 +1,19 @@
+// Soil Moisture Sensor Test (Arduino UNO)
+#define SOIL_PIN A0
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int raw = analogRead(SOIL_PIN);
+  int moisture = map(raw, 1023, 300, 0, 100);
+  moisture = constrain(moisture, 0, 100);
+
+  Serial.print("Raw Value: ");
+  Serial.print(raw);
+  Serial.print("  |  Moisture %: ");
+  Serial.println(moisture);
+
+  delay(1000);
+}
